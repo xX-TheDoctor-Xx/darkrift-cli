8478f75d0137e3ac1bf24704aa66f9ab43ef5b58
The Doctor <xX-TheDoctor-Xx@users.noreply.github.com>
2020-06-25 09:55:35 +0100

    Cant delete the running dll but it can be replaced
    Needs testing in linux

---
+++ test_upgrade/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace test_upgrade
+{
+    internal class Program
+    {
+        public static string GetApplicationRoot()
+        {
+            var exePath = Path.GetDirectoryName(System.Reflection
+                              .Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return appRoot;
+        }
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                //File.Delete("test.upgrade.dll");
+
+                string path = Path.Combine(GetApplicationRoot(), "test_upgrade.dll");
+                Console.WriteLine(path);
+                File.Move("C:\\Users\\Utilizador\\Desktop\\darkrift-cli\\test_upgrade\\bin\\Debug\\test_upgrade.dll", path, true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("nope");
+                Console.WriteLine(e.Message);
+                if (e.InnerException != null)
+                    Console.WriteLine(e.InnerException.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
